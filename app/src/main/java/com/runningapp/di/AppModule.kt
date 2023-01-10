acac3c62e836cedb9e0c829c428c441824a70207
Viacheslav Panasiuk <badgerle82@gmail.com>
2023-01-10 11:36:53 +0200

    Saving name and weight

--- app/src/main/java/com/runningapp/di/AppModule.kt
+++ app/src/main/java/com/runningapp/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.runningapp.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.runningapp.db.RunningDB
+import com.runningapp.util.Constants.KEY_FIRST_TIME_TOGGLE
+import com.runningapp.util.Constants.KEY_NAME
+import com.runningapp.util.Constants.KEY_WEIGHT
 import com.runningapp.util.Constants.RUNNING_DB_NAME
+import com.runningapp.util.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,4 +34,24 @@ object AppModule {
     @Provides
     @Singleton
     fun provideRunDao(db: RunningDB) = db.getRunDao()
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPref: SharedPreferences) =
+        sharedPref.getString(KEY_NAME, "") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPref: SharedPreferences) =
+        sharedPref.getFloat(KEY_WEIGHT, 50f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPref: SharedPreferences) =
+        sharedPref.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
 }
